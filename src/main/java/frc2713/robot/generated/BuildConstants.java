56064e3bc00760f5513f7139436567b7468b0185
Cameron Orlandella <Cameron@orlandella.net>
2026-02-14 11:50:46 -0500

    NeutralScoreNeutral

--- src/main/java/frc2713/robot/generated/BuildConstants.java
+++ src/main/java/frc2713/robot/generated/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 59;
-  public static final String GIT_SHA = "43c1641fd266bc9e320a573bcd13d04cc92273a2";
-  public static final String GIT_DATE = "2026-02-09 20:32:40 EST";
+  public static final int GIT_REVISION = 60;
+  public static final String GIT_SHA = "8449774bedecc0b31e37828b01885bbc968aa498";
+  public static final String GIT_DATE = "2026-02-14 11:41:49 EST";
   public static final String GIT_BRANCH = "cam/autopaths-commands";
-  public static final String BUILD_DATE = "2026-02-13 19:15:11 EST";
-  public static final long BUILD_UNIX_TIME = 1771028111731L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-02-14 11:49:07 EST";
+  public static final long BUILD_UNIX_TIME = 1771087747714L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
