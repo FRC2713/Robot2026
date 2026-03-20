efe58ae4a8840e1545fefe05000579e3476528af
2713driverstation <frc2713@gmail.com>
2026-03-20 19:20:00 -0400

    revert demo changes

--- src/main/java/frc2713/robot/RobotContainer.java
+++ src/main/java/frc2713/robot/RobotContainer.java
@@ -35,13 +35,16 @@ import frc2713.robot.oi.DriverControls;
 import frc2713.robot.oi.OperatorControls;
 import frc2713.robot.subsystems.drive.Drive;
 import frc2713.robot.subsystems.drive.GyroIO;
+import frc2713.robot.subsystems.drive.GyroIOPigeon2;
 import frc2713.robot.subsystems.drive.ModuleIO;
 import frc2713.robot.subsystems.drive.ModuleIOSim;
+import frc2713.robot.subsystems.drive.ModuleIOTalonFX;
 import frc2713.robot.subsystems.intake.IntakeConstants;
 import frc2713.robot.subsystems.intake.IntakeExtension;
 import frc2713.robot.subsystems.intake.IntakeRoller;
 import frc2713.robot.subsystems.intake.intakeExtensionIO.IntakeExtensionIO;
 import frc2713.robot.subsystems.intake.intakeExtensionIO.IntakeExtensionIOSim;
+import frc2713.robot.subsystems.intake.intakeExtensionIO.IntakeExtensionIOTalonFX;
 import frc2713.robot.subsystems.launcher.Flywheels;
 import frc2713.robot.subsystems.launcher.Hood;
 import frc2713.robot.subsystems.launcher.LauncherConstants;
@@ -103,11 +106,11 @@ public class RobotContainer {
 
         drive =
             new Drive(
-                new GyroIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {});
+                new GyroIOPigeon2(),
+                new ModuleIOTalonFX(TunerConstants.FrontLeft),
+                new ModuleIOTalonFX(TunerConstants.FrontRight),
+                new ModuleIOTalonFX(TunerConstants.BackLeft),
+                new ModuleIOTalonFX(TunerConstants.BackRight));
         flywheels =
             new Flywheels(
                 LauncherConstants.Flywheels.leaderConfig,
@@ -126,12 +129,15 @@ public class RobotContainer {
 
         intakeRoller =
             new IntakeRoller(
-                new TalonFXSubsystemConfig(),
-                new TalonFXSubsystemConfig(),
-                new MotorIO() {},
-                new MotorIO() {});
+                IntakeConstants.Roller.leaderConfig,
+                IntakeConstants.Roller.followerConfig,
+                new TalonFXIO(IntakeConstants.Roller.leaderConfig),
+                new TalonFXIO(IntakeConstants.Roller.followerConfig));
+
         intakeExtension =
-            new IntakeExtension(new TalonFXSubsystemConfig(), new IntakeExtensionIO() {});
+            new IntakeExtension(
+                IntakeConstants.Extension.config,
+                new IntakeExtensionIOTalonFX(IntakeConstants.Extension.differentialConfig));
 
         dyeRotor =
             new DyeRotor(
