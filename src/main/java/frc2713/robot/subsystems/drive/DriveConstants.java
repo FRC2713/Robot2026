e9f0c9254ec750f0e23f36e44d4afc67aabc894a
Unnas Hussain <uwh.547@gmail.com>
2026-03-26 11:06:50 -0400

    auto heading controller matches tuned teleop controller

--- src/main/java/frc2713/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc2713/robot/subsystems/drive/DriveConstants.java
@@ -14,7 +14,7 @@ public class DriveConstants {
             "positionTraj", new Slot0Configs().withKP(1.7).withKD(0.08), new MotionMagicConfigs());
     public static final LoggedTunableGains headingTrajectoryController =
         new LoggedTunableGains(
-            "headingTraj", new Slot0Configs().withKP(5.7).withKD(0.0), new MotionMagicConfigs());
+            "headingTraj", new Slot0Configs().withKP(6.5).withKD(0.2), new MotionMagicConfigs());
   }
 
   public static final LoggedTunableNumber speedScalar =
