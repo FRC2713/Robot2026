db7c885d795d1e5007209c98f3cbc3967ffcc49c
RHR2713 <frc2713@gmail.com>
2026-03-28 12:34:37 -0400

    Update at 'Sat Mar 28 12:34:37 EDT 2026'

--- src/main/java/frc2713/robot/GameCommandGroups.java
+++ src/main/java/frc2713/robot/GameCommandGroups.java
@@ -54,7 +54,7 @@ public final class GameCommandGroups {
                   dyeRotor.feedWhenReady(
                       () -> flywheels.atTarget() && hood.atTarget(), Seconds.of(0.8)),
                   extension.maintainFuelPressureCommand(1).beforeStarting(Commands.waitSeconds(1))),
-              () -> FieldConstants.NeutralZone.region.contains(drive.getPose().getTranslation()))
+              () -> !FieldConstants.AllianceZone.getAllianceRegion().contains(drive.getPose().getTranslation()))
           .withName("Auto OTF Shooting");
     }
 
